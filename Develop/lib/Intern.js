66502e084abb88aa37ef06bb4c2fa910fdc683d3
Ryan Hull <ryandelonhull@gmail.com>
2020-10-12 23:08:05 -0400

    add classes to employee, manager, intern, and engineer

---
+++ Develop/lib/Intern.js
@@ -0,0 +1,17 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern {
+    constructor (name, id, email) {
+        super(name, id, email);
+        this.school = school;
+    }
+
+    getSchool(){
+        return this.school;
+    }
+
+    getRole() {
+        return "Intern";
+    }
+}
+
+module.exports = Manager;
---
+++ Develop/lib/Manager.js
@@ -0,0 +1,12 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class manager {
+    constructor (name, id, email) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getRole() {
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
